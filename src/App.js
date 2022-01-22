0cf0a6ace850885edea3e6b52223e8c4d47db5d2
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2022-01-23 01:29:42 +0500

    1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+ 
 
 function App() {
+  
+  const [input, setInput] = useState(null)
+  const [print, setPrint] = useState(false)
+  
   return (
-    <div className="App">
-      
-    </div>
-  );
-}
+
+   
+<div>
+  {print? <h1> {input} </h1> : null} 
+
+  <input value={input} onChange={e => setInput(e.target.value) }/>
+  <button onClick={() => {setPrint(true)}}> кнопка </button>
+
+
+</div>
+
+
+
+      )}
 
 export default App;
