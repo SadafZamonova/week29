c29361864e162b21a973c04e177cef63d2ef11f8
SadafZamonova <84777664+SadafZamonova@users.noreply.github.com>
2022-01-26 01:38:52 +0500

    добавила массив сверху

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import React, { useState } from 'react';
 
 function App() {
   
-  const [input, setInput] = useState(null)
-  const [print, setPrint] = useState(false)
-  
-  // addTop = ( ) => {
-  //   setPrint (input + print )
-  // }
-  return (
-
+  const [input, setInput] = useState("") // 
+  const [addprint, setAddPrint] = useState([]) //массив, куда должны попадать сообщения
+ 
+  //функция, которая добавляет value inputа в массив комментов и очищает инпут
+ const addTop = ( ) => {
+   const result = [...addprint]; //создается массив с комментами
+   result.unshift(input); //добавление нового коммента в начало массива
+   setAddPrint(result); //перезаписывание 
+   setInput(""); //очищение поля инпута
+};
    
-<div>
-  {print? <h1> {input} </h1> : null} 
+  return (  
 
-  <input value={input} onChange={e => setInput(e.target.value) }/>
-  <button onClick={() => {setPrint (input)}}> кнопка </button>
+<div >
+ <div >
+ { addprint.map((print,index) => (<p  key={`${index}`}> {print} </p>))  }
+ 
+   </div>
+  
+  <input 
+      value={input} 
+      onChange={e => setInput(e.target.value) }/>
+  <button onClick={addTop}> кнопка </button>
 
 
 </div>
